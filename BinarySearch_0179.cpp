f7a4dffc014f59e38f8a252014b17a7670739f7a
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-17 14:58:19 +0700

    menambahkan void input

--- BinarySearch_0179.cpp
+++ BinarySearch_0179.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int element[10];
+int nPanjang;
+int x;
+
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n=======================================\n";
+    cout << "    Masukkan Elemen Array             \n";
+    cout << "=====================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
