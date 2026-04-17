b2ebef7dae00498797798603efaec7e9d4c4baf7
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-17 15:10:37 +0700

    menambahkan for di void display

--- BinarySearch_0179.cpp
+++ BinarySearch_0179.cpp
@@ -58,4 +58,15 @@ void display()
     cout << " Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "========================================\n";
 
+for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+
+    cout << endl;
 }
+
